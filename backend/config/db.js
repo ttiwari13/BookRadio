5b510d81969069ae9682cb46d20324692ae23efc
Tanishka Tiwari <126980091+ttiwari13@users.noreply.github.com>
2025-07-03 15:21:01 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Atlas connected");
+    console.log(" MongoDB Atlas connected");
   } catch (err) {
-    console.error("❌ Connection failed:", err.message);
+    console.error(" Connection failed:", err.message);
     process.exit(1);
   }
 };
